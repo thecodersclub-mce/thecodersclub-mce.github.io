3c8d2a38e1d3a359592bab0d09d67a630ab48302
thecodersclub-mce <thecodersclubmce@gmail.com>
2025-03-29 10:44:05 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.querySelector('.menu-toggle');
     const menuBar = document.querySelector('.menu-bar');
-    const secondaryMenu = document.querySelector('.secondary-menu');
 
     toggleButton.addEventListener('click', () => {
-        // Hide the secondary menu when the toggle button is pressed
-        secondaryMenu.style.display = secondaryMenu.style.display === 'none' ? 'flex' : 'none';
-
-        // Toggle visibility of the menu-bar
-        menuBar.classList.toggle('show');
+        menuBar.style.display = menuBar.style.display === 'none' ? 'flex' : 'none';
     });
 });
