f46495523966349f90e836b39217b19d408a2dfc
thecodersclub-mce <thecodersclubmce@gmail.com>
2025-03-29 10:46:17 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.querySelector('.menu-toggle');
     const menuBar = document.querySelector('.menu-bar');
+    const secondaryMenu = document.querySelector('.secondary-menu');
 
     toggleButton.addEventListener('click', () => {
-        menuBar.style.display = menuBar.style.display === 'none' ? 'flex' : 'none';
+        secondaryMenu.style.display = secondaryMenu.style.display === 'none' ? 'flex' : 'none';
+
+        menuBar.classList.toggle('show');
     });
 });
